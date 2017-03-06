6a931c4c6bdfce4e432005749f7a6d01564be04a
MG <mattgordon@hotmail.co.uk>
2017-03-06 09:51:12 +0000

    static

--- src/components/reactTasks/champs.js
+++ src/components/reactTasks/champs.js
@@ -4,10 +4,10 @@ export default class Champ extends React.Component {
   render() {
     return (
 	
-        
       
-      <div style={{background: "url(' + this.props.file ')"}}>
-
+      
+      <div>
+	 <img src={this.props.file} /> 
         <p>Name: { this.props.champ.name } </p>
         <p>Role: { this.props.champ.role } </p>
         <p>Difficulty: { this.props.champ.diff } </p>
--- src/components/reactTasks/index.js
+++ src/components/reactTasks/index.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Champ from './champs'
+import React from 'react';
+import Champ from './champs';
 
 
 export default () => {
   return (
     <div>
-      <Champ file={'/AatroxSquare.png'} champ={{ name: 'Aatrox', role: 'Fighter', diff: '6/10', price: '6300ip' }} />
-      <Champ champ={{ name: 'Ahri', role: 'Assassin', diff: '8/10', price: '6300ip' }} />
+      <Champ file={'./components/reactTasks/AatroxSquare.png'} champ={{ name: 'Aatrox', role: 'Fighter', diff: '6/10', price: '6300ip' }} />
+      
+	<Champ file={'./components/reactTasks/AhriSquare.png'} champ={{ name: 'Ahri', role: 'Assassin', diff: '8/10', price: '6300ip' }} />
 
-    </div>
+        <Champ file={'./components/reactTasks/BardSquare.png'} champ={{ name: 'Bard', role: 'Support', diff: '9/10', price: '6300ip' }} />   
+
+        <Champ file={'./components/reactTasks/GangplankSquare.png'} champ={{ name: 'Gangplank', role: 'Fighter', diff: '8/10', price: '3150ip' }} />
+        
+        <Champ file={'./components/reactTasks/GarenSquare.png'} champ={{ name: 'Garen', role: 'Tank', diff: '2/10', price: '450ip' }} />
+
+        <Champ file={'./components/reactTasks/EzrealSquare.png'} champ={{ name: 'Ezreal', role: 'Marksman', diff: '7/10', price: '4800ip' }} />
+
+        <Champ file={'./components/reactTasks/ZileanSquare.png'} champ={{ name: 'Zilean', role: 'Support/Mage', diff: '6/10', price: '1350ip' }} />
+</div>
   )
 }
 
