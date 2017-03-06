9f574193f3c591674c581298359b4cf3b55ec57e
MG <mattgordon@hotmail.co.uk>
2017-03-06 11:28:42 +0000

    champarray

--- src/components/reactTasks/champs.js
+++ src/components/reactTasks/champs.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 export default class Champ extends React.Component {
   render() {
-    return (
-	
+const champion = function(champ) {
+        return (<div>
+        <img src={champ.file} /> 
+        <p>Name: {champ.name } </p>
+        <p>Role: { champ.role } </p>
+        <p>Difficulty: { champ.diff } </p>
+        <p>Price: { champ.price } </p> 
+         </div>);
+}
+return (
+    
      
       
       <div>
-	 <img src={this.props.file} /> 
-        <p>Name: { this.props.champ.name } </p>
-        <p>Role: { this.props.champ.role } </p>
-        <p>Difficulty: { this.props.champ.diff } </p>
-        <p>Price: { this.props.champ.price } </p>
+         Champions: { _.map(this.props.Champions, champion) }
+     
       </div>
-	
+    
     )
   }
 }
--- src/components/reactTasks/index.js
+++ src/components/reactTasks/index.js
@@ -7,20 +7,15 @@ export default () => {
     <div>
       <h1>Champs</h1>
 
-      <Champ file={'./components/reactTasks/AatroxSquare.png'} champ={{ name: 'Aatrox', role: 'Fighter', diff: '6/10', price: '6300ip' }} />
-      
-	<Champ file={'./components/reactTasks/AhriSquare.png'} champ={{ name: 'Ahri', role: 'Assassin', diff: '8/10', price: '6300ip' }} />
+      <Champ  Champions={[{ name: 'Aatrox', role: 'Fighter', diff: '6/10', price: '6300ip', file: './components/reactTasks/AatroxSquare.png' }, 
+{ name: 'Ahri', role: 'Assassin', diff: '8/10', price: '6300ip', file: './components/reactTasks/AhriSquare.png' }, 
+{ name: 'Bard', role: 'Support', diff: '9/10', price: '6300ip', file: './components/reactTasks/BardSquare.png' },    
+{ name: 'Gangplank', role: 'Fighter', diff: '8/10', price: '3150ip', file: './components/reactTasks/GangplankSquare.png' },
+{ name: 'Garen', role: 'Tank', diff: '2/10', price: '450ip', file: './components/reactTasks/GarenSquare.png' },
+{ name: 'Ezreal', role: 'Marksman', diff: '7/10', price: '4800ip', file: './components/reactTasks/EzrealSquare.png' },
+{ name: 'Zilean', role: 'Support/Mage', diff: '6/10', price: '1350ip', file: './components/reactTasks/ZileanSquare.png' }]} />
 
-        <Champ file={'./components/reactTasks/BardSquare.png'} champ={{ name: 'Bard', role: 'Support', diff: '9/10', price: '6300ip' }} />   
-
-        <Champ file={'./components/reactTasks/GangplankSquare.png'} champ={{ name: 'Gangplank', role: 'Fighter', diff: '8/10', price: '3150ip' }} />
-        
-        <Champ file={'./components/reactTasks/GarenSquare.png'} champ={{ name: 'Garen', role: 'Tank', diff: '2/10', price: '450ip' }} />
-
-        <Champ file={'./components/reactTasks/EzrealSquare.png'} champ={{ name: 'Ezreal', role: 'Marksman', diff: '7/10', price: '4800ip' }} />
-
-        <Champ file={'./components/reactTasks/ZileanSquare.png'} champ={{ name: 'Zilean', role: 'Support/Mage', diff: '6/10', price: '1350ip' }} />
-</div>
-  )
+ </div>
+  );
 }
 
